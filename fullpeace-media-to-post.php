ea26b49232f0de3580bc4a579b85c46a34663d5a
Calle Hunefalk <hunefalk@gmail.com>
2014-08-08 19:44:30 +0200

    Version 0.1.1

--- fullpeace-media-to-post.php
+++ fullpeace-media-to-post.php
@@ -20,7 +20,7 @@
  * Plugin Name:       FullPeace Media To Post
  * Plugin URI:        http://github.com/FullPeace/fullpeace-media-to-post
  * Description:       FullPeace Media To Post creates Custom Post Types for media files on upload via the Media Library.
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            FullPeace.org
  * Author URI:        http://fullpeace.org
  * Text Domain:       fullpeace-media-to-posts
